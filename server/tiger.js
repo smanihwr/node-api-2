236de1a90c27b24452339ec330ca4016ee2dbf4e
smanihwr <smanihwr@gmail.com>
2018-12-19 18:21:21 -0600

    refactored tiger route

--- server/tiger.js
+++ server/tiger.js
@@ -7,7 +7,7 @@ tigerRouter.param('id', (req, res, next, id) => {
     var index = _.findIndex(tigers, {id: id});
     var tiger = tigers[index];
 
-    if(tiger) {
+    if (tiger) {
         req.tiger = tiger;
         next();
     } else {
@@ -15,53 +15,50 @@ tigerRouter.param('id', (req, res, next, id) => {
     }
 });
 
-tigerRouter.get('/', (req, res) => {
-    // to test error middleware
-    //throw new Error('Custom error');
-
-    res.json(tigers);
-});
-
-tigerRouter.get('/:id', (req, res) => {
-    res.json(req.tiger);
-});
-
 incrementId = (req, res, next) => {
     req.body.id = ++id + '';
     next();
 };
 
-tigerRouter.post('/', incrementId, (req, res) => {
-    var tiger = req.body;
-    tigers.push(tiger);
-    res.json(tiger);
-});
+tigerRouter.route('/')
+    .get((req, res) => {
+        // to test error middleware
+        //throw new Error('Custom error');
+        res.json(tigers);
+    })
+    .post(incrementId, (req, res) => {
+        var tiger = req.body;
+        tigers.push(tiger);
+        res.json(tiger);
+    });
 
-tigerRouter.put('/:id', (req, res) => {
+tigerRouter.route('/:id')
+    .get((req, res) => {
+        res.json(req.tiger);
+    })
+    .put((req, res) => {
 
-    let update = req.body;
-    if(update.id) {
-        delete update.id;
-    }
+        let update = req.body;
+        if (update.id) {
+            delete update.id;
+        }
 
-    if(!req.tiger) {
-        res.send();
-    } else {
-        var updatedTiger = _.assign(req.tiger, update);
-        res.json(updatedTiger);
-    }
-});
-
-
-tigerRouter.delete('/:id', (req, res) => {
-    var tiger = req.tiger;
-    if(!tiger) {
-        res.send();
-    } else {
-        var deletedTiger = tiger;
-        tigers.splice(tiger, 1)
-        res.json(deletedTiger);
-    }
-});
+        if (!req.tiger) {
+            res.send();
+        } else {
+            var updatedTiger = _.assign(req.tiger, update);
+            res.json(updatedTiger);
+        }
+    })
+    .delete((req, res) => {
+        var tiger = req.tiger;
+        if (!tiger) {
+            res.send();
+        } else {
+            var deletedTiger = tiger;
+            tigers.splice(tiger, 1)
+            res.json(deletedTiger);
+        }
+    });
 
 module.exports = tigerRouter;
